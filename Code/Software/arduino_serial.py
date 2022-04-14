e8e5b2899f09e7af97582b03e3e0e53424a5e296
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2022-04-14 19:50:03 +0430

    add output support to hw.

--- Code/Software/arduino_serial.py
+++ Code/Software/arduino_serial.py
@@ -1,3 +1,4 @@
+import queue
 import threading
 import serial
 import random
@@ -6,8 +7,10 @@ import time
 SAMPLE_DELAY = 10  # milli
 buffer_lock = threading.Lock()
 adc_inputs = []
+pwm_queue = queue.Queue()
 
 
+####  REAL  ####
 def read_adc(ser: serial.SerialBase):
     try:
         sample = ser.readline().strip()
@@ -15,8 +18,18 @@ def read_adc(ser: serial.SerialBase):
             sample = int(sample)
             with buffer_lock:
                 adc_inputs.append(sample)
+                pwm_queue.put(sample)  # Just for test
     except Exception as e:
         print(e)
+
+
+def write_adc(ser: serial.SerialBase):
+    with buffer_lock:
+        if pwm_queue.empty():
+            val = 0
+        else:
+            val = pwm_queue.get()
+    ser.write(str.encode('{}\n'.format(val)))
 
 
 def start_read_adc_thread():
@@ -24,15 +37,16 @@ def start_read_adc_thread():
         ser = serial.Serial(port='/dev/cu.usbmodem14201', baudrate=115200, timeout=1)
         while True:
             read_adc(ser)
+            write_adc(ser)
 
     threading.Thread(target=continuous_read_adc).start()
 
-# fake (for test)
+####  FAKE   ####
 # x = 500
 # s = 0
 #
 #
-# def read_adc(ser):
+# def read_adc():
 #     global x, s
 #     time.sleep(SAMPLE_DELAY / 1000)
 #     s += random.randint(-3, 3)
@@ -41,4 +55,24 @@ def start_read_adc_thread():
 #     x += s
 #     x = min(x, 1024)
 #     x = max(x, 0)
-#     adc_inputs.append(x)
+#     with buffer_lock:
+#         adc_inputs.append(x)
+#         pwm_queue.put(x)  # Just for test
+#
+#
+# def write_adc():
+#     with buffer_lock:
+#         if pwm_queue.empty():
+#             val = 500
+#         else:
+#             val = pwm_queue.get()
+#     print('Output: ', val)
+#
+#
+# def start_read_adc_thread():
+#     def continuous_read_adc():
+#         while True:
+#             read_adc()
+#             write_adc()
+#
+#     threading.Thread(target=continuous_read_adc).start()
