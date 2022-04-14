80e8af346945de2b6bdf45c0083f246b813dbeb9
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2022-04-14 19:38:35 +0430

    add output support to hw.

--- Code/Hardware/sketch_0/sketch_0.ino
+++ Code/Hardware/sketch_0/sketch_0.ino
@@ -1,11 +1,23 @@
+int ledPin = 2;
+String str;
+int x;
+
 void setup() {
-  Serial.begin(9600);          //  setup serial
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
+  delay(10);
   
-  int val = analogRead(0);    // read the input pin
-  Serial.println(val);             // debug value
+  int val = analogRead(0);
+  Serial.println(val);
+
+  
+  if (Serial.available() > 0) {
+    str = Serial.readStringUntil('\n');
+    x = Serial.parseInt();
+    analogWrite(ledPin, x / 4); 
+    // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
+  }
 }
