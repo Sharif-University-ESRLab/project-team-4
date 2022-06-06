95bc423896a766a916241029e0a6736dadd4ca25
mahsa.ama <mahsa.ama1391@gmail.com>
2022-06-06 17:59:27 +0430

    comments added to arduino_serial.py

--- Code/Software/arduino_serial.py
+++ Code/Software/arduino_serial.py
@@ -4,20 +4,30 @@ import serial
 import random
 import time
 
+# number of channels
 NUM = 8
+
+# time between sampling intervals
 SAMPLE_DELAY = 50  # milli
+
+# lock for reading and writing data in their respective queues
 buffer_lock = threading.Lock()
+
+# arrays of inputs and pwm outputs for each channel
 adc_inputs = [[] for _ in range(NUM)]
 pwm_queues = [queue.Queue() for _ in range(NUM)]
 
 
 ####  REAL  ####
+# read data from input port in arduino
 def read_adc(ser: serial.SerialBase):
     try:
         packet = ser.readline().strip().decode("utf-8")
-        samples = packet.split('|')
+        # seperate input data with |
+        samples = packet.split("|")
         if len(samples) != (NUM + 1):
             return
+        # get the buffer to save input values into
         with buffer_lock:
             for i in range(NUM):
                 if samples[i].isnumeric():
@@ -26,25 +36,31 @@ def read_adc(ser: serial.SerialBase):
         print(e)
 
 
+# write saved values to the serial port(pwm output)
 def write_adc(ser: serial.SerialBase):
-    packet = ''
+    packet = ""
+    # get the buffer to put output values into
     with buffer_lock:
         for i in range(NUM):
+            # if no value is available in the queue, so LED is off
             if pwm_queues[i].empty():
                 val = 0
             else:
                 val = pwm_queues[i].get()
-            packet += '{:4}|'.format(val)
-    ser.write(str.encode('{}\n'.format(packet)))
+            packet += "{:4}|".format(val)
+    ser.write(str.encode("{}\n".format(packet)))
 
 
+# define a thread for continuously reading and writing data from and to arduino
 def start_read_adc_thread():
+    # infinite loop to read and write
     def continuous_read_adc():
-        ser = serial.Serial(port='/dev/cu.usbmodem14201', baudrate=115200, timeout=1)
+        ser = serial.Serial(port="/dev/cu.usbmodem14201", baudrate=115200, timeout=1)
         while True:
             read_adc(ser)
             write_adc(ser)
 
+    # start a thread running above function
     threading.Thread(target=continuous_read_adc).start()
 
 
